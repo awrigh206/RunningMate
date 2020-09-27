357257d33a06127cf405d637e040755dc7cba1e1
awrigh206 <awrigh206@caledonian.ac.uk>
2020-09-27 12:13:26 +0100

    Refactoring code for better readability

---
+++ lib/CustomWidgets/LoginForm.dart
@@ -0,0 +1,234 @@
+import 'dart:developer';
+
+import 'package:application/Helpers/TcpHelper.dart';
+import 'package:application/Models/Payload.dart';
+import 'package:application/Models/User.dart';
+import 'package:application/Routes/UserView.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:password/password.dart';
+import 'package:password_strength/password_strength.dart';
+
+class LoginForm extends StatefulWidget {
+  LoginForm({Key key, @required this.tcp, @required this.update})
+      : super(key: key);
+  final TcpHelper tcp;
+  final Function update;
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  bool isRegistering = false;
+  bool authentication = false;
+  bool processing = false;
+  @override
+  void dispose() {
+    // Clean up the controller
+    userNameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  bool getProcessing() {
+    return processing;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              controller: userNameController,
+              decoration: const InputDecoration(
+                hintText: 'User Name',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter your username';
+                }
+                if (value.contains(' ')) {
+                  return 'Please do not use spaces';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              autocorrect: false,
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                double passwordStrength =
+                    estimatePasswordStrength(passwordController.text);
+                if (passwordStrength < 0.3) {
+                  return 'Your password is too weak';
+                }
+                if (value.contains(' ')) {
+                  return 'Please do not use spaces';
+                }
+                return null;
+              },
+            ),
+            Builder(builder: (BuildContext context) {
+              if (isRegistering) {
+                return Column(
+                  children: [
+                    TextFormField(
+                      autocorrect: false,
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                      ),
+                      validator: (value) {
+                        String email = emailController.text;
+                        if (!EmailValidator.validate(email)) {
+                          return 'That email is not valid';
+                        }
+                        if (value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        if (value.contains(' ')) {
+                          return 'Please do not use spaces';
+                        }
+                        return null;
+                      },
+                    ),
+                    CheckboxListTile(
+                        value: isRegistering,
+                        title: Text('Register'),
+                        onChanged: (bool value) {
+                          //so  something  when box is changed
+                          setState(() {
+                            isRegistering = value;
+                          });
+                        }),
+                  ],
+                );
+              } else {
+                return CheckboxListTile(
+                    value: isRegistering,
+                    title: Text('Register'),
+                    onChanged: (bool value) {
+                      //so  something  when box is changed
+                      setState(() {
+                        isRegistering = value;
+                      });
+                    });
+              }
+            }),
+            Builder(builder: (BuildContext context) {
+              return Column(
+                children: [
+                  //LocalAuthentication(),
+                ],
+              );
+            }),
+            ButtonBar(
+              children: [
+                RaisedButton(
+                    onPressed: () async {
+                      await widget.update;
+                      if (formKey.currentState.validate()) {
+                        login();
+                      }
+                    },
+                    child: Text('Submit')),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> login() async {
+    String password = Password.hash(passwordController.text, new PBKDF2());
+    User user =
+        new User(userNameController.text, password, emailController.text);
+    User userEncrypted =
+        User(userNameController.text, password, emailController.text);
+    await userEncrypted.encryptDetails(); //protect user details
+    bool userExists = await this
+        .widget
+        .tcp
+        .sendPayloadBoolean(new Payload(userEncrypted.toJson(), 'exists'));
+    if (isRegistering && !userExists) {
+      this
+          .widget
+          .tcp
+          .sendPayload(new Payload(userEncrypted.toJson(), 'register'));
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => UserView(
+                  currentUser: user,
+                )),
+      );
+    } else {
+      authentication = await this
+          .widget
+          .tcp
+          .sendPayloadBoolean(Payload(userEncrypted.toJson(), 'login'));
+      if (authentication) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => UserView(
+                    currentUser: user,
+                  )),
+        );
+      } else {
+        log("no authentication");
+      }
+    }
+    if (userExists && isRegistering) {
+      //display message that the user already exists in the  system
+      log("user exists");
+      showTheDialog();
+    }
+  }
+
+  Future<void> showTheDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('User already exists'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Please choose a different user name'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
--- lib/Routes/LoginView.dart
+++ lib/Routes/LoginView.dart
@@ -1,15 +1,9 @@
-import 'dart:developer';
+import 'package:application/CustomWidgets/LoginForm.dart';
 import 'package:application/Helpers/LocationHelper.dart';
 import 'package:application/Helpers/TcpHelper.dart';
-import 'package:application/Models/Payload.dart';
-import 'package:application/Models/User.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:password/password.dart';
 import 'MapView.dart';
-import 'UserView.dart';
-import 'package:password_strength/password_strength.dart';
-import 'package:email_validator/email_validator.dart';
 import 'package:lottie/lottie.dart';
 
 class LoginView extends StatefulWidget {
@@ -23,21 +17,24 @@ class LoginView extends StatefulWidget {
 class LoginViewState extends State<LoginView> with TickerProviderStateMixin {
   final locationHelper = LocationHelper();
   final TcpHelper tcp = TcpHelper();
-  final userNameController = TextEditingController();
-  final passwordController = TextEditingController();
-  final emailController = TextEditingController();
-  final formKey = GlobalKey<FormState>();
-  bool isRegistering = false;
   Color currentColor = Colors.white;
   Future<LottieComposition> composition;
   AnimationController loginAnimation;
-  bool processing = false;
   Lottie animation;
+  LoginForm form;
+  bool processing = false;
 
   @override
   void initState() {
     super.initState();
     composition = fetchAnimation();
+    form = new LoginForm(tcp: tcp, update: update);
+  }
+
+  void update() {
+    setState(() {
+      processing = true;
+    });
   }
 
   Future<LottieComposition> fetchAnimation() async {
@@ -50,16 +47,12 @@ class LoginViewState extends State<LoginView> with TickerProviderStateMixin {
   void dispose() {
     // Clean up the controller when the widget is removed from the
     // widget tree.
-    userNameController.dispose();
-    emailController.dispose();
-    passwordController.dispose();
     loginAnimation?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    bool authentication = false;
     return Scaffold(
       backgroundColor: currentColor,
       appBar: AppBar(
@@ -70,177 +63,24 @@ class LoginViewState extends State<LoginView> with TickerProviderStateMixin {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Form(
-                key: formKey,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        autocorrect: false,
-                        controller: userNameController,
-                        decoration: const InputDecoration(
-                          hintText: 'User Name',
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your username';
-                          }
-                          if (value.contains(' ')) {
-                            return 'Please do not use spaces';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        autocorrect: false,
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          hintText: 'Password',
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          double passwordStrength =
-                              estimatePasswordStrength(passwordController.text);
-                          if (passwordStrength < 0.3) {
-                            return 'Your password is too weak';
-                          }
-                          if (value.contains(' ')) {
-                            return 'Please do not use spaces';
-                          }
-                          return null;
-                        },
-                      ),
-                      Builder(builder: (BuildContext context) {
-                        if (processing) {
-                          return FutureBuilder(
-                              future: composition,
-                              builder: (context, snapshot) {
-                                if (snapshot.hasData) {
-                                  return Lottie(composition: snapshot.data);
-                                } else {
-                                  return Container();
-                                }
-                              });
-                        }
-                        if (isRegistering) {
-                          return Column(
-                            children: [
-                              TextFormField(
-                                autocorrect: false,
-                                controller: emailController,
-                                decoration: const InputDecoration(
-                                  hintText: 'Email',
-                                ),
-                                validator: (value) {
-                                  String email = emailController.text;
-                                  if (!EmailValidator.validate(email)) {
-                                    return 'That email is not valid';
-                                  }
-                                  if (value.isEmpty) {
-                                    return 'Please enter your email';
-                                  }
-                                  if (value.contains(' ')) {
-                                    return 'Please do not use spaces';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              CheckboxListTile(
-                                  value: isRegistering,
-                                  title: Text('Register'),
-                                  onChanged: (bool value) {
-                                    //so  something  when box is changed
-                                    setState(() {
-                                      isRegistering = value;
-                                    });
-                                  }),
-                            ],
+              Builder(builder: (BuildContext context) {
+                if (processing) {
+                  return FutureBuilder(
+                      future: composition,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Container(
+                            child: Lottie(composition: snapshot.data),
+                            color: Colors.deepPurple,
                           );
                         } else {
-                          return CheckboxListTile(
-                              value: isRegistering,
-                              title: Text('Register'),
-                              onChanged: (bool value) {
-                                //so  something  when box is changed
-                                setState(() {
-                                  isRegistering = value;
-                                });
-                              });
+                          return CircularProgressIndicator();
                         }
-                      }),
-                      Builder(builder: (BuildContext context) {
-                        return Column(
-                          children: [
-                            //LocalAuthentication(),
-                          ],
-                        );
-                      }),
-                      ButtonBar(
-                        children: [
-                          RaisedButton(
-                              onPressed: () async {
-                                setState(() {
-                                  processing = true;
-                                });
-                                if (formKey.currentState.validate()) {
-                                  String password = Password.hash(
-                                      passwordController.text, new PBKDF2());
-                                  User user = new User(userNameController.text,
-                                      password, emailController.text);
-                                  User userEncrypted = User(
-                                      userNameController.text,
-                                      password,
-                                      emailController.text);
-                                  await userEncrypted
-                                      .encryptDetails(); //protect user details
-                                  bool userExists =
-                                      await tcp.sendPayloadBoolean(new Payload(
-                                          userEncrypted.toJson(), 'exists'));
-                                  if (isRegistering && !userExists) {
-                                    tcp.sendPayload(new Payload(
-                                        userEncrypted.toJson(), 'register'));
-
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => UserView(
-                                                currentUser: user,
-                                              )),
-                                    );
-                                  } else {
-                                    authentication =
-                                        await tcp.sendPayloadBoolean(Payload(
-                                            userEncrypted.toJson(), 'login'));
-                                    if (authentication) {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => UserView(
-                                                  currentUser: user,
-                                                )),
-                                      );
-                                    } else {
-                                      log("no authentication");
-                                    }
-                                  }
-                                  if (userExists && isRegistering) {
-                                    //display message that the user already exists in the  system
-                                    log("user exists");
-                                    showTheDialog();
-                                  }
-                                }
-                              },
-                              child: Text('Submit')),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+                      });
+                } else {
+                  return form;
+                }
+              }),
             ],
           ),
         ),
@@ -255,33 +95,6 @@ class LoginViewState extends State<LoginView> with TickerProviderStateMixin {
         tooltip: "Go to google maps with your location",
         child: Icon(Icons.map),
       ),
-    );
-  }
-
-  Future<void> showTheDialog() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('User already exists'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('Please choose a different user name'),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('OK'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
     );
   }
 }
