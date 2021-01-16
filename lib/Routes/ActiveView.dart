697473cbf3527541e3658199a12c4cf6b861f183
awrigh206 <awrigh206@caledonian.ac.uk>
2021-01-16 18:43:55 +0000

    Fix logic errors

--- lib/Routes/ActiveView.dart
+++ lib/Routes/ActiveView.dart
@@ -1,13 +1,9 @@
 import 'dart:async';
-
 import 'package:application/DTO/UpdateDto.dart';
 import 'package:application/Helpers/HttpHelper.dart';
 import 'package:application/Helpers/LocationHelper.dart';
-import 'package:application/Helpers/TcpHelper.dart';
 import 'package:application/Models/Distance.dart';
 import 'package:application/Models/Pair.dart';
-import 'package:application/Models/Payload.dart';
-import 'package:application/Models/Position.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:location/location.dart';
@@ -24,17 +20,25 @@ class _ActiveViewState extends State<ActiveView> {
   Timer timer;
   Distance distanceTravelled;
   LocationHelper locationHelper = new LocationHelper();
-  LocationData currentPosition;
-  LocationData lastPosition;
+  Future<LocationData> currentPosition;
+  Future<LocationData> lastPosition;
   GetIt getIt = GetIt.instance;
 
   @override
   void initState() {
+    beginRun();
+    currentPosition = locationHelper.getLocationBasic();
     super.initState();
     const duration = const Duration(seconds: 2);
     timer = Timer.periodic(duration, (timer) async {
       await sendData();
     });
+  }
+
+  Future<void> beginRun() async {
+    HttpHelper httpHelper = getIt<HttpHelper>();
+    final res = await httpHelper.postRequest(
+        getIt<String>() + 'run', this.widget.currentPair.toJson());
   }
 
   Future<void> sendData() async {
@@ -43,18 +47,18 @@ class _ActiveViewState extends State<ActiveView> {
         this.widget.currentPair.challengedUser;
     //Code in this function body is run every two seconds
     lastPosition = currentPosition;
-    currentPosition = await locationHelper.getLocationBasic();
+    currentPosition = locationHelper.getLocationBasic();
     Pair pair = this.widget.currentPair;
-    UpdateDto updateDto = UpdateDto(
-        pair, calculateDistance(lastPosition, currentPosition), 0.0, 2.0);
+    UpdateDto updateDto = UpdateDto(pair,
+        calculateDistance(await lastPosition, await currentPosition), 0.0, 2.0);
     httpHelper.putRequest(getIt<String>() + 'run/update', updateDto.toJson());
   }
 
   //calculate difference between two points using the haversine formula
   double calculateDistance(LocationData previous, LocationData current) {
     int radius = 6371; //radius of the earth in km
-    var dlat = convertToRadian(currentPosition.latitude - previous.latitude);
-    var dlon = convertToRadian(currentPosition.longitude - previous.longitude);
+    var dlat = convertToRadian(current.latitude - previous.latitude);
+    var dlon = convertToRadian(current.longitude - previous.longitude);
 
     var a = sin(dlat / 2) * sin(dlat / 2) +
         cos(convertToRadian(previous.latitude)) *
